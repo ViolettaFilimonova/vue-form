82ace93f9b540c7619334bebbc760567308fb98b
ViolettaFilimonova <violetta99.filimonova@mail.ru>
2022-03-29 15:35:12 +0300

    working login

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,47 @@
 import { createStore } from 'vuex'
+import router from  '../router'
+import {auth} from "../firebase";
+import { signInWithEmailAndPassword, signOut} from 'firebase/auth'
 
 export default createStore({
   state: {
+    user: null
   },
   mutations: {
+    SET_USER (state, user){
+      state.user = user
+    },
+    CLEAR_USER (state){
+      state.user = null
+    }
   },
   actions: {
-  },
-  modules: {
+    async login({commit}, details){
+      const {email, password} = details
+
+      try {
+        await signInWithEmailAndPassword(auth, email, password)
+      }catch (error){
+        switch (error.code){
+          case 'auth/user-not-found':
+            alert('Пользователь не найден')
+                break
+          case 'auth/wrong-password':
+            alert('Неправильный пароль')
+            break
+          default:
+            alert(error.message)
+        }
+        return
+      }
+      commit('SET_USER', auth.currentUser)
+
+      router.push('/')
+    },
+    async logout({commit}){
+      await signOut(auth)
+      commit('CLEAR_USER')
+      router.push('/login')
+    }
   }
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="login">
+    <section class="form">
+      <form  class="login" @submit.prevent="login ">
+        <input type="text" placeholder="Номер договора" v-model="login_form.email">
+        <input type="password " placeholder="Пароль" v-model="login_form.password">
+        <input type="submit" value="Войти" >
+      </form>
+    </section>
+  </main>
+</template>
+
+<script>
+import {ref} from "vue";
+import {useStore} from "vuex"
+
+export default {
+  setup(){
+    const login_form = ref({});
+    const store = useStore();
+
+    const login = () => {
+      store.dispatch('login', login_form.value)
+    }
+
+    return{
+      login_form,
+      login
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
