a42f894887403afdc76d6b5df7f1998ca69ab789
ViolettaFilimonova <violetta99.filimonova@mail.ru>
2022-03-29 11:19:02 +0300

    vue create

---
+++ src/firebase/index.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+ import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyABN_WGknOIoiMVvi0gZ9oa9YPFjabYfg0",
+    authDomain: "login-d4f41.firebaseapp.com",
+    projectId: "login-d4f41",
+    storageBucket: "login-d4f41.appspot.com",
+    messagingSenderId: "977997640442",
+    appId: "1:977997640442:web:a650bcbaba2eacc3b04f43",
+    measurementId: "G-8MD5WKJLL0"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+export {auth}
